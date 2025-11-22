5291a8a9b9eaa1607908716edf65fa773e8b00bf
mooven <gabinlegrand56@gmail.com>
2025-11-22 14:57:50 +0100

    feat: added laws at the bottom of the menu, and also added sounds effects

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -122,6 +122,9 @@ export default function Main(props) {
                     </Card>
                 </div>
             </div>
+        <div className="w-100 opaque-dark-blue py-1" >
+            LoLSauce is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games and all associated properties are trademarks or registered trademarks of Riot Games, Inc
+        </div>
         </div>
     </>
     )
--- front/src/pages/Room/Player/RoomPlayerMenu.jsx
+++ front/src/pages/Room/Player/RoomPlayerMenu.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import UserCard from './UserCard';
 import { useSelector } from 'react-redux';
+import pentaKillMP3 from '../../../assets/sounds/Killstreak_SFX_Multikill_Melody_Penta.mp3';
+import firstKillMP3 from '../../../assets/sounds/Killstreak_SFX_Multikill_Point_First.mp3';
 
 export function RoomPlayerMenu(props) {
 
     const users = useSelector(state => state.room.users)
     const userId = useSelector(state => state.auth.userId)
+    const [alreadyPlayedUsersAudio, setAlreadyPlayedUsersAudio] = React.useState([])
 
-    const handleRenderUsers = () => {
+    const isNewQuestion = () => {
+        let result = true
+        users.map((user) => {
+            if(user.success){
+                result = false
+            }
+        })
+        return result
+    }
+
+    const handleRenderUsers = React.useMemo(() => {
         let usersCopy = [...users]
         usersCopy.sort(function(x,y){
             if(x.score >= y.score){
@@ -16,14 +29,26 @@ export function RoomPlayerMenu(props) {
                 return 1
             }
         })
-        return usersCopy.map((user, index) => (
-            <UserCard key={index} index={index} user={user} isCurrentUser={userId === user.user_id}/>
-        ))
-    }
+        if(isNewQuestion()){
+            setAlreadyPlayedUsersAudio([])
+        }
+        return usersCopy.map((user, index) => {
+            if(user.success && userId === user.user_id && alreadyPlayedUsersAudio.length === 0){
+                let audio = new Audio(pentaKillMP3);
+                audio.play();!alreadyPlayedUsersAudio.includes(user.user_id)
+            }
+            if(user.success && !alreadyPlayedUsersAudio.includes(user.user_id)){
+                let audio = new Audio(firstKillMP3);
+                audio.play();
+                setAlreadyPlayedUsersAudio([...alreadyPlayedUsersAudio, user.user_id])
+            }
+            return <UserCard key={index} index={index} user={user} isCurrentUser={userId === user.user_id}/>
+        })
+    },[users])
 
     return (
         <div className="d-flex flex-column align-items-start gap-2 p-1 overflow-y-scroll overflow-x-hidden h-100" style={{backgroundColor:"rgba(41, 45, 70, 0.8)", width:"30vw", minWidth:"150px", maxWidth:"300px"}}>
-            {handleRenderUsers()}
+            {handleRenderUsers}
         </div>
     )
 }
