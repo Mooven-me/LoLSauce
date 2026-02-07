831f13bd818dd73b9e076cd1e0a8dd4b832434f6
Mooven-me <gabinlegrand56@gmail.com>
2026-02-07 16:26:09 +0000

    fixed configuration on some others servers

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
 import symfonyPlugin from 'vite-plugin-symfony';
 import path from 'path'
 
@@ -9,20 +8,24 @@ export default defineConfig(({mode}) => {
 
   return {
   plugins: [
-      react(),
       symfonyPlugin({
           refresh: true,
       })
   ],
 
+  esbuild: {
+      jsx: 'automatic',
+    },
+
+
   root: '.',
 
   server: {
     port: 5173,
-    origin: env.VITE_SERVER_NAME+':5173',
+    origin: env.VITE_SERVER_NAME,
     strictPort: true,
     cors: {
-      origin: env.VITE_SERVER_NAME,
+      origin: env.VITE_PUBLIC_DOMAIN,
       credentials: true,
     },
     watch: {
